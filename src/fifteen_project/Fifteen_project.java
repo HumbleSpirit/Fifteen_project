9e639e8f4acf6d1b84bc1c989671e6890f297134
Boro_s99 <Boro_s99@221-12.od.itstep.local>
2017-07-31 18:56:43 +0300

    lists

---
+++ src/fifteen_project/Fifteen_project.java
@@ -0,0 +1,18 @@
+        
+package fifteen_project;
+import java.util.ArrayList;
+
+public class Fifteen_project {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 44;
+        String y = "Hello";
+        ArrayList list = new ArrayList();
+        list.add(x);
+        list.add(y);
+        int z = (int)list.get(0);
+        System.out.println(z);
+    }
+    
+}
